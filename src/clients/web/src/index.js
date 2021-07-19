87e53939a266a3603a2ffdf64c51009b4599a431
Ryan Sites <ryan.sites@optum.com>
2021-07-19 08:29:53 -0400

    login working

---
+++ src/clients/web/src/index.js
@@ -0,0 +1,67 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.10.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import {
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed,
+} from "@react-firebase/auth";
+import firebase from "firebase";
+import { config } from "./config";
+
+// core components
+import Admin from "layouts/Admin.js";
+
+import "assets/css/material-dashboard-react.css?v=1.10.0";
+
+export const App = () => {
+  return (
+    <FirebaseAuthProvider {...config} firebase={firebase}>
+      <div>
+        <IfFirebaseAuthed>
+          {() => {
+            return (
+              <BrowserRouter>
+                <Switch>
+                  <Route path="/admin" component={Admin} />
+                  <Redirect from="/" to="/admin/dashboard" />
+                </Switch>
+              </BrowserRouter>
+            );
+          }}
+        </IfFirebaseAuthed>
+        <IfFirebaseUnAuthed>
+          {() => {
+            return (
+              <BrowserRouter>
+                <Switch>
+                  <Route path="/admin" component={Admin} />
+                  <Redirect from="/" to="/admin/home" />
+                </Switch>
+              </BrowserRouter>
+            );
+          }}
+        </IfFirebaseUnAuthed>
+      </div>
+    </FirebaseAuthProvider>
+  );
+};
+render(<App />, document.getElementById("root"));
---
+++ src/clients/web/src/routes.js
@@ -0,0 +1,123 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.10.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Home from "@material-ui/icons/Home";
+import Login from "@material-ui/icons/VerifiedUser";
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+// core components/views for Admin layout
+import HomePage from "views/Home/Home.js";
+import LoginPage from "views/Login/Login.js";
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import UserProfile from "views/UserProfile/UserProfile.js";
+import TableList from "views/TableList/TableList.js";
+import Typography from "views/Typography/Typography.js";
+import Icons from "views/Icons/Icons.js";
+import Maps from "views/Maps/Maps.js";
+import NotificationsPage from "views/Notifications/Notifications.js";
+// core components/views for RTL layout
+
+const dashboardRoutes = [
+  {
+    path: "/home",
+    name: "Home",
+    rtlName: "",
+    icon: Home,
+    component: HomePage,
+    layout: "/admin",
+    protected: false,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    rtlName: "",
+    icon: Login,
+    component: LoginPage,
+    layout: "/admin",
+    protected: false,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/user",
+    name: "User Profile",
+    rtlName: "",
+    icon: Person,
+    component: UserProfile,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    rtlName: "",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    rtlName: "",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    rtlName: "",
+    icon: BubbleChart,
+    component: Icons,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    rtlName: "",
+    icon: LocationOn,
+    component: Maps,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "",
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: "/admin",
+    protected: true,
+  },
+];
+
+export default dashboardRoutes;
